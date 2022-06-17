e6dc88a0db19a283730780e59d39acf947db77c0
TessShearer <shearer.tess@gmail.com>
2022-06-17 11:44:52 -0600

    more detailed file structre, beginning handlebars, beginning models and routes

---
+++ models/Index.js
@@ -0,0 +1,71 @@
+// Everything here is copied from the just-tech-news assignment for my reference when writing my own
+
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const Like = require('./Like');
+const Comment = require('./Comment');
+
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+User.belongsToMany(Post, {
+  through: Like,
+  as: 'Liked_posts',
+
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Post.belongsToMany(User, {
+  through: Like,
+  as: 'Liked_posts',
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+Like.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Like.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(Like, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Like, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Like, Comment };
---
+++ models/Like.js
@@ -0,0 +1,39 @@
+// Everything here is copied from the just-tech-news assignment for my reference when writing my own
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Like extends Model {}
+
+Like.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Like'
+  }
+);
+
+module.exports = Like;
